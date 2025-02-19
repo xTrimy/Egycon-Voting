1bd8586ddd6a9556ba64a2ea7e23cefc99ce6bf0
xTrimy <Mohamed.ashraf881999@gmail.com>
2025-02-19 03:25:37 +0200

    Add fillable properties and user relationship to TelegramChat model

--- app/Models/TelegramChat.php
+++ app/Models/TelegramChat.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class TelegramChat extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'chat_id',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
