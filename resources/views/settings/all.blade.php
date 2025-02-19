a7e57c62aa39c93d674058d65b814cce50118797
xTrimy <Mohamed.ashraf881999@gmail.com>
2025-02-19 03:27:52 +0200

    Fix route for disabling Telegram notifications in settings view

--- resources/views/settings/all.blade.php
+++ resources/views/settings/all.blade.php
@@ -34,7 +34,7 @@ Settings
             </h1>
             <h2 class="text-lg">Receive system notifications as telegram messages.</h2>
             @if($telegramNotificationsEnabled)
-                <a href="{{ route('admin.settings.disableTelegram') }}">
+                <a href="{{ route('settings.disableTelegram') }}">
                     <button type="button" class="table items-center mt-4 justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-red-600 border border-transparent rounded-lg active:bg-red-600 hover:bg-red-700 focus:outline-none focus:shadow-outline-purple">
                         Disable notifications
                     </button>
